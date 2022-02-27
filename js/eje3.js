4a2f85f0fb4881ff8c8ed0c553928abf829f9585
agMalek <albermaleh.99@gmail.com>
2022-02-27 14:44:51 -0300

    sort eje3, con extras

--- js/eje3.js
+++ js/eje3.js
@@ -6,22 +6,46 @@ const getRandomNumber = (desde, hasta) => {
 const palosIcon = ["♠", "♦", "♣", "♥"]
 const palosName = ["spade", "diamond", "club", "heart"]
 const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
+const tiempoCartaAutomatica = 10000
 
 window.onload = () => {
+    crearCarta()
+}
+
+const crearCarta = () =>{
     const indexPalos = getRandomNumber(0, 3)
     let iconoPalo = palosIcon[indexPalos]
     let nombrePalo = palosName[indexPalos]
 
     let numero = numeros[getRandomNumber(0, 12)]
-    document.querySelector(".card").className += ` ${nombrePalo}`
-    document.querySelector(".card").innerHTML = 
-    `
-        <div class="paloSuperior">${iconoPalo}</div>
-        <div class="numeroCard">${numero}</div>
-        <div class="paloInferior">${iconoPalo}</div>
-
+    document.querySelector(".cards-container").innerHTML += 
+    ` 
+        <div class="card">
+            <div class="paloSuperior ${nombrePalo}">${iconoPalo}</div>
+            <div class="numeroCard">${numero}</div>
+            <div class="paloInferior ${nombrePalo}">${iconoPalo}</div>
+        </div>
     `
 }
 
+const actualizarMedidas = () => {
+    let cards = document.querySelectorAll(".card")
+    console.log(cards)
+    cards.forEach(card => (
+        cambiarMedidas(card)        
+    ))
+}
 
+const cambiarMedidas = (card) => {
+    let heigth = document.querySelector("#heigth").value
+    let width = document.querySelector("#width").value
+    console.log(heigth)
+    card.style.width = `${width}px`
+    card.style.height = `${heigth}px`
+}
+
+
+setInterval(() => {
+    let interval = crearCarta()
+    return clearInterval(interval)
+}, tiempoCartaAutomatica);
